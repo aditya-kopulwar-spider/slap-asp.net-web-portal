1548889627670f28ad8b8493c817809c54dda8d7
Ajay Chavan <achavan@spiderlogic.com>
2017-12-19 13:41:21 +0530

    Appraisal Process View, Controller Added

---
+++ SLAP-App/SLAP-App/Controllers/AppraisalProcessesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SLAP_Data;
+
+namespace SLAP_App.Controllers
+{
+    public class AppraisalProcessesController : Controller
+    {
+        private slap_dbEntities db = new slap_dbEntities();
+
+        // GET: AppraisalProcesses
+        public ActionResult Index()
+        {
+            return View(db.AppraisalProcesses.ToList());
+        }
+
+        // GET: AppraisalProcesses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AppraisalProcess appraisalProcess = db.AppraisalProcesses.Find(id);
+            if (appraisalProcess == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appraisalProcess);
+        }
+
+        // GET: AppraisalProcesses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AppraisalProcesses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+//        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AppraisalProcessId,AppraisalProcessYear,IsActive,PeerListFinalizationByDate,SendPeerFeedbackRequestByDate,SendPeerFeedbackByDate,SelfAppraisalSubmissionByDate,AppraisalMeetingByDate,GoalSettingByDate")] AppraisalProcess appraisalProcess)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AppraisalProcesses.Add(appraisalProcess);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(appraisalProcess);
+        }
+
+        // GET: AppraisalProcesses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AppraisalProcess appraisalProcess = db.AppraisalProcesses.Find(id);
+            if (appraisalProcess == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appraisalProcess);
+        }
+
+        // POST: AppraisalProcesses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+//        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AppraisalProcessId,AppraisalProcessYear,IsActive,PeerListFinalizationByDate,SendPeerFeedbackRequestByDate,SendPeerFeedbackByDate,SelfAppraisalSubmissionByDate,AppraisalMeetingByDate,GoalSettingByDate")] AppraisalProcess appraisalProcess)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(appraisalProcess).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(appraisalProcess);
+        }
+
+        // GET: AppraisalProcesses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AppraisalProcess appraisalProcess = db.AppraisalProcesses.Find(id);
+            if (appraisalProcess == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appraisalProcess);
+        }
+
+        // POST: AppraisalProcesses/Delete/5
+        [HttpPost, ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AppraisalProcess appraisalProcess = db.AppraisalProcesses.Find(id);
+            db.AppraisalProcesses.Remove(appraisalProcess);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SLAP-App/SLAP-App/Models/AppraisalSeasonModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLAP_App.Models
+{
+    public class AppraisalSeasonModel
+    {
+        public int AppraisalProcessId { get; set; }
+        public short AppraisalProcessYear { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime PeerListFinalizationByDate { get; set; }
+        public DateTime SendPeerFeedbackRequestByDate { get; set; }
+        public DateTime SendPeerFeedbackByDate { get; set; }
+        public DateTime SelfAppraisalSubmissionByDate { get; set; }
+        public DateTime AppraisalMeetingByDate { get; set; }
+        public DateTime GoalSettingByDate { get; set; }
+    }
+}
